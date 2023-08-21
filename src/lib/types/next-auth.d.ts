f88ef70c83d0748b3eb8360b0b93d6f6e451f123
Bhagya Mudgal <bhagyamudgal@gmail.com>
2023-08-21 23:08:17 +0530

    chore: add admin wallets env

--- src/lib/types/next-auth.d.ts
+++ src/lib/types/next-auth.d.ts
@@ -1,11 +1,12 @@
 import type { DefaultSession } from "next-auth";
 import type { DefaultJWT } from "next-auth/jwt";
 
-import type { User } from "@/src/db/schema";
-
 declare module "next-auth" {
     interface Session extends DefaultSession {
-        user: User;
+        user: {
+            walletAddress: string;
+            role: "admin";
+        };
     }
 }
 
