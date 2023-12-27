eb97d1e31f3437afe45a4e56e48ade610a0b93c4
Bhagya Mudgal <bhagyamudgal@gmail.com>
2023-12-27 22:10:13 +0530

    feat: add filter sort and search

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,17 @@
 /* eslint-disable sonarjs/no-duplicate-string */
-import { Box, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import {
+    Box,
+    Center,
+    HStack,
+    Input,
+    Select,
+    SimpleGrid,
+    Text,
+    VStack,
+} from "@chakra-ui/react";
 import format from "date-fns/format";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 import ProtectedRoute from "@/src/components/auth/ProtectedRoute";
 import CustomContainer from "@/src/components/common/CustomContainer";
@@ -14,6 +24,64 @@ export const DEFAULT_DATE_FORMAT = "dd MMMM yyyy hh:mm a";
 
 const RenderUsersPage = () => {
     const { isLoading, isFetching, data, isError, refetch } = useUsers();
+
+    const [selectedStatusFilter, setSelectedStatusFilter] = useState<
+        "all" | "active" | "expired"
+    >("all");
+    const [selectedSort, setSelectedSort] = useState<"latest" | "oldest">(
+        "latest"
+    );
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    const [filteredData, setFilteredData] = useState(data);
+
+    useEffect(() => {
+        const fData = data?.filter((user) => {
+            if (selectedStatusFilter === "all") {
+                return true;
+            }
+            return user.nftStatus === selectedStatusFilter;
+        });
+
+        const sData = fData?.sort((a, b) => {
+            if (selectedSort === "latest") {
+                return (
+                    new Date(b.createdAt).getTime() -
+                    new Date(a.createdAt).getTime()
+                );
+            }
+
+            return (
+                new Date(a.createdAt).getTime() -
+                new Date(b.createdAt).getTime()
+            );
+        });
+
+        const searchData = sData?.filter((d) => {
+            const lowerCaseSearchTerm = searchTerm.toLowerCase();
+
+            if (d.name?.toLowerCase()?.includes(lowerCaseSearchTerm)) {
+                return true;
+            }
+
+            if (d.email?.toLowerCase()?.includes(lowerCaseSearchTerm)) {
+                return true;
+            }
+
+            if (d.walletAddress?.toLowerCase()?.includes(lowerCaseSearchTerm)) {
+                return true;
+            }
+
+            // eslint-disable-next-line sonarjs/prefer-single-boolean-return
+            if (d.discordId?.toLowerCase()?.includes(lowerCaseSearchTerm)) {
+                return true;
+            }
+
+            return false;
+        });
+
+        setFilteredData(searchData);
+    }, [data, selectedStatusFilter, selectedSort, searchTerm]); // eslint-disable-line
 
     if (isLoading || isFetching) {
         return <PageDataLoading loadingText="Loading Users" />;
@@ -29,128 +97,196 @@ const RenderUsersPage = () => {
         );
     }
 
-    if (data.length === 0) {
+    if (data?.length === 0) {
         return <PageDataNotFound message="No users found!" />;
     }
 
     return (
-        <SimpleGrid columns={{ base: 1, lg: 2 }} gap={6}>
-            {data.map((user) => {
-                return (
-                    <VStack
-                        w="100%"
-                        key={user.id}
-                        border="2px"
-                        borderColor="white"
-                        borderRadius="10px"
-                        p={4}
-                        alignItems="flex-start"
-                        spacing={6}
-                        wordBreak="break-all"
+        <VStack alignItems="flex-start" w="100%" spacing={6}>
+            <HStack w="100%" justifyContent="flex-end" spacing={6}>
+                <VStack alignItems="flex-start" flexGrow={1}>
+                    <Text fontWeight={600}>
+                        Search (By name, wallet, email, discord)
+                    </Text>
+                    <Input
+                        placeholder="Enter here"
+                        onChange={(e) => {
+                            setSearchTerm(e.target.value);
+                        }}
+                    />
+                </VStack>
+                <VStack alignItems="flex-start">
+                    <Text fontWeight={600}>Filter by Status</Text>
+                    <Select
+                        defaultValue="all"
+                        onChange={(e) => {
+                            setSelectedStatusFilter(
+                                e.target.value as "all" | "active" | "expired"
+                            );
+                        }}
                     >
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>User ID</Text>
-                            <Text color="purple.400">{user.id}</Text>
+                        <option value="all">All</option>
+                        <option value="active">Active</option>
+                        <option value="expired">Expired</option>
+                    </Select>
+                </VStack>
+                <VStack alignItems="flex-start">
+                    <Text fontWeight={600}>Sort By Time</Text>
+                    <Select
+                        defaultValue="latest"
+                        onChange={(e) => {
+                            setSelectedSort(
+                                e.target.value as "latest" | "oldest"
+                            );
+                        }}
+                    >
+                        <option value="latest">Latest</option>
+                        <option value="oldest">Oldest</option>
+                    </Select>
+                </VStack>
+            </HStack>
+
+            <HStack w="100%" justifyContent="flex-start" spacing={6}>
+                <VStack alignItems="flex-start">
+                    <Text fontWeight={600}>Total Users</Text>
+                    <Text color="purple.400">{data?.length}</Text>
+                </VStack>
+                <VStack alignItems="flex-start">
+                    <Text fontWeight={600}>Filtered Users</Text>
+                    <Text color="purple.400">{filteredData?.length}</Text>
+                </VStack>
+            </HStack>
+
+            {filteredData?.length === 0 && (
+                <Center w="100%">
+                    <PageDataNotFound message="No data found for this filter!" />
+                </Center>
+            )}
+
+            <SimpleGrid columns={{ base: 1, lg: 2 }} gap={6} w="100%">
+                {filteredData?.map((user) => {
+                    return (
+                        <VStack
+                            w="100%"
+                            key={user.id}
+                            border="2px"
+                            borderColor="white"
+                            borderRadius="10px"
+                            p={4}
+                            alignItems="flex-start"
+                            spacing={6}
+                            wordBreak="break-all"
+                        >
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>User ID</Text>
+                                <Text color="purple.400">{user.id}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Name</Text>
+                                <Text color="purple.400">{user.name}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Wallet Address</Text>
+                                <Text color="purple.400">
+                                    {user.walletAddress}
+                                </Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Email</Text>
+                                <Text color="purple.400">{user.email}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Discord Username</Text>
+                                <Text color="purple.400">{user.discordId}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Country</Text>
+                                <Text color="purple.400">{user.country}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Role</Text>
+                                <Text color="purple.400">{user.role}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>NFT Type</Text>
+                                <Text color="purple.400">{user.nftType}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>
+                                    Underdog NFT Status
+                                </Text>
+                                <Text color="purple.400">{user.nftStatus}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Underdog NFT ID</Text>
+                                <Text color="purple.400">{user.nftId}</Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>
+                                    Underdog NFT Issued At
+                                </Text>
+                                <Text color="purple.400">
+                                    {user.nftIssuedAt
+                                        ? format(
+                                              new Date(user.nftIssuedAt),
+                                              "MMM dd, yyyy hh:mm a"
+                                          )
+                                        : "N/A"}
+                                </Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>
+                                    Underdog NFT Expires At
+                                </Text>
+                                <Text color="purple.400">
+                                    {user.nftExpiresAt
+                                        ? format(
+                                              new Date(user.nftExpiresAt),
+                                              "MMM dd, yyyy hh:mm a"
+                                          )
+                                        : "N/A"}
+                                </Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>
+                                    Underdog NFT Renewed At
+                                </Text>
+                                <Text color="purple.400">
+                                    {user.nftRenewedAt
+                                        ? format(
+                                              new Date(user.nftRenewedAt),
+                                              "MMM dd, yyyy hh:mm a"
+                                          )
+                                        : "N/A"}
+                                </Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Creation Date</Text>
+                                <Text color="purple.400">
+                                    {user.createdAt
+                                        ? format(
+                                              new Date(user.createdAt),
+                                              "MMM dd, yyyy hh:mm a"
+                                          )
+                                        : "N/A"}
+                                </Text>
+                            </VStack>
+                            <VStack w="100%" alignItems="flex-start">
+                                <Text fontWeight={600}>Update Date</Text>
+                                <Text color="purple.400">
+                                    {user.updatedAt
+                                        ? format(
+                                              new Date(user.updatedAt),
+                                              "MMM dd, yyyy hh:mm a"
+                                          )
+                                        : "N/A"}
+                                </Text>
+                            </VStack>
                         </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Name</Text>
-                            <Text color="purple.400">{user.name}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Wallet Address</Text>
-                            <Text color="purple.400">{user.walletAddress}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Email</Text>
-                            <Text color="purple.400">{user.email}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Discord Username</Text>
-                            <Text color="purple.400">{user.discordId}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Country</Text>
-                            <Text color="purple.400">{user.country}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Role</Text>
-                            <Text color="purple.400">{user.role}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>NFT Type</Text>
-                            <Text color="purple.400">{user.nftType}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Underdog NFT Status</Text>
-                            <Text color="purple.400">{user.nftStatus}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Underdog NFT ID</Text>
-                            <Text color="purple.400">{user.nftId}</Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Underdog NFT Issued At</Text>
-                            <Text color="purple.400">
-                                {user.nftIssuedAt
-                                    ? format(
-                                          new Date(user.nftIssuedAt),
-                                          "MMM dd, yyyy hh:mm a"
-                                      )
-                                    : "N/A"}
-                            </Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>
-                                Underdog NFT Expires At
-                            </Text>
-                            <Text color="purple.400">
-                                {user.nftExpiresAt
-                                    ? format(
-                                          new Date(user.nftExpiresAt),
-                                          "MMM dd, yyyy hh:mm a"
-                                      )
-                                    : "N/A"}
-                            </Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>
-                                Underdog NFT Renewed At
-                            </Text>
-                            <Text color="purple.400">
-                                {user.nftRenewedAt
-                                    ? format(
-                                          new Date(user.nftRenewedAt),
-                                          "MMM dd, yyyy hh:mm a"
-                                      )
-                                    : "N/A"}
-                            </Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Creation Date</Text>
-                            <Text color="purple.400">
-                                {user.createdAt
-                                    ? format(
-                                          new Date(user.createdAt),
-                                          "MMM dd, yyyy hh:mm a"
-                                      )
-                                    : "N/A"}
-                            </Text>
-                        </VStack>
-                        <VStack w="100%" alignItems="flex-start">
-                            <Text fontWeight={600}>Update Date</Text>
-                            <Text color="purple.400">
-                                {user.updatedAt
-                                    ? format(
-                                          new Date(user.updatedAt),
-                                          "MMM dd, yyyy hh:mm a"
-                                      )
-                                    : "N/A"}
-                            </Text>
-                        </VStack>
-                    </VStack>
-                );
-            })}
-        </SimpleGrid>
+                    );
+                })}
+            </SimpleGrid>
+        </VStack>
     );
 };
 
